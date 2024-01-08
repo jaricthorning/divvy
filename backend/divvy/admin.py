d3f155da915f54243bc37d72b1dc96ff7d641dfb
Jaric Thorning <jaric.thorning@gmail.com>
2024-01-09 00:02:57 +1100

    Added basic Expense model to begin

--- backend/divvy/admin.py
+++ backend/divvy/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Expense
 
 # Register your models here.
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'paidBy')
+
+admin.site.register(Expense, ExpenseAdmin)
